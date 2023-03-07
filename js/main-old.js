c4f8643629585a5f924b58ddf2cf8bf1d2e71e98
tomaszgrazul <tgra@wp.pl>
2023-03-07 16:24:44 +0100

    dodana sekcja kontakt

---
+++ js/main-old.js
@@ -0,0 +1,46 @@
+document.getElementsByClassName('switch-main')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    // document.getElementsByClassName('main-section')[0].style.opacity = '1';
+    // document.getElementsByClassName('akrylowe')[0].style.opacity = '0';
+    // document.getElementsByClassName('olejne')[0].style.opacity = '0';
+    document.getElementsByClassName('main-section')[0].classList.remove('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+});
+
+document.getElementsByClassName('switch-akrylowe')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    // document.getElementsByClassName('akrylowe')[0].classList.toggle('switch-akrylowe-class');
+    // document.getElementsByClassName('main-section')[0].classList.toggle('switch-main-class');
+    // document.getElementsByClassName('main-section')[0].style.opacity = '0';
+    // document.getElementsByClassName('akrylowe')[0].style.opacity = '1';
+    // document.getElementsByClassName('olejne')[0].style.opacity = '0';
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.add('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+});
+
+document.getElementsByClassName('switch-olejne')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    console.log(document.getElementsByClassName('switch-olejne'));
+    // document.getElementsByClassName('akrylowe')[0].classList.toggle('switch-akrylowe-class');
+    // document.getElementsByClassName('main-section')[0].classList.toggle('switch-main-class');
+    // document.getElementsByClassName('olejne')[0].style.opacity = '1';
+    // document.getElementsByClassName('main-section')[0].style.opacity = '0';
+    // document.getElementsByClassName('akrylowe')[0].style.opacity = '0';
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.add('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+
+});
+
+document.getElementsByClassName('switch-szkice')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.add('switch-szkice-on');
+})
--- js/main.js
+++ js/main.js
@@ -1,39 +1,46 @@
 document.getElementsByClassName('switch-main')[0].addEventListener('click', function(element) {
     element.preventDefault();
-    // document.getElementsByClassName('main-section')[0].style.opacity = '1';
-    // document.getElementsByClassName('akrylowe')[0].style.opacity = '0';
-    // document.getElementsByClassName('olejne')[0].style.opacity = '0';
     document.getElementsByClassName('main-section')[0].classList.remove('switch-main-off');
     document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
     document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
     document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
 });
 
 document.getElementsByClassName('switch-akrylowe')[0].addEventListener('click', function(element) {
     element.preventDefault();
-    // document.getElementsByClassName('akrylowe')[0].classList.toggle('switch-akrylowe-class');
-    // document.getElementsByClassName('main-section')[0].classList.toggle('switch-main-class');
-    // document.getElementsByClassName('main-section')[0].style.opacity = '0';
-    // document.getElementsByClassName('akrylowe')[0].style.opacity = '1';
-    // document.getElementsByClassName('olejne')[0].style.opacity = '0';
     document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
     document.getElementsByClassName('akrylowe')[0].classList.add('switch-akrylowe-on');
     document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
     document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
+});
+
+document.getElementsByClassName('switch-akrylowe-1')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.add('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
 });
 
 document.getElementsByClassName('switch-olejne')[0].addEventListener('click', function(element) {
     element.preventDefault();
-    // document.getElementsByClassName('akrylowe')[0].classList.toggle('switch-akrylowe-class');
-    // document.getElementsByClassName('main-section')[0].classList.toggle('switch-main-class');
-    // document.getElementsByClassName('olejne')[0].style.opacity = '1';
-    // document.getElementsByClassName('main-section')[0].style.opacity = '0';
-    // document.getElementsByClassName('akrylowe')[0].style.opacity = '0';
     document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
     document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
     document.getElementsByClassName('olejne')[0].classList.add('switch-olejne-on');
     document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
+});
 
+document.getElementsByClassName('switch-olejne-1')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.add('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
 });
 
 document.getElementsByClassName('switch-szkice')[0].addEventListener('click', function(element) {
@@ -42,4 +49,23 @@ document.getElementsByClassName('switch-szkice')[0].addEventListener('click', fu
     document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
     document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
     document.getElementsByClassName('szkice')[0].classList.add('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
+});
+
+document.getElementsByClassName('switch-szkice-1')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.add('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.remove('switch-contact-on');
+});
+
+document.getElementsByClassName('switch-contact')[0].addEventListener('click', function(element) {
+    element.preventDefault();
+    document.getElementsByClassName('main-section')[0].classList.add('switch-main-off');
+    document.getElementsByClassName('akrylowe')[0].classList.remove('switch-akrylowe-on');
+    document.getElementsByClassName('olejne')[0].classList.remove('switch-olejne-on');
+    document.getElementsByClassName('szkice')[0].classList.remove('switch-szkice-on');
+    document.getElementsByClassName('contact')[0].classList.add('switch-contact-on');
 })
